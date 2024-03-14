ac698ae190d2dfe07f8d6ca7a5d6086d175a7f72
vsjc-valuers <it@vsjadon.com>
2024-03-14 16:59:33 +0530

    added redux feature

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,9 +1,22 @@
-import { UseSelector } from "react-redux";
+import { UseSelector, useSelector } from "react-redux";
+import { selectAllPosts } from "./postsSlice";
 
 const PostsList = () => {
-  return (
-    <div>PostsList</div>
-  )
-}
+  const posts = useSelector(selectAllPosts);
 
-export default PostsList
+  const renderedPosts = posts.map((post) => (
+    <article key={post.id}>
+      <h3> {post.title} </h3>
+      <p>{post.content.substring(0, 100)}</p>
+    </article>
+  ));
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {renderedPosts}
+    </section>
+  );
+};
+
+export default PostsList;
